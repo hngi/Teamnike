2f9d2402a96a3fe996836215b5f86123b4337f3d
Adtrex <38886632+Adtrex@users.noreply.github.com>
2019-10-04 12:46:50 +0100

    Add files via upload

--- action.js
+++ action.js
@@ -9,6 +9,17 @@ let hours_use = document.querySelector('#hours_use');
 let electDetails = document.querySelector('#electDetails');
 
 
+//FOR THE EDIT FEATURES
+//inputs
+let elect_edit = document.querySelector('#elect_edit');
+let power_edit = document.querySelector('#power_edit');
+let hours_edit = document.querySelector('#hours_edit');
+
+//divs 
+let inputDiv = document.querySelector('.inputform');
+let editInputDiv = document.querySelector('.outputform');
+
+
 
 //output button
 let calcu_btn = document.querySelector('#calcu_btn');
@@ -18,6 +29,7 @@ let domtotal = document.querySelector('#total');
 
 //add button
 let add_btn = document.querySelector('#add_btn');
+let edit_btn = document.querySelector('#edit_btn');
 
 //output variables
 let result = document.querySelector('#result');
@@ -54,19 +66,25 @@ let addItems = () => {
     if(powerval && hoursInt && electName.length){
         elId = powerObj.length;
         //let elId = 0;
+        let editBtn = `
+        <button id = "" class="btn btn-danger editbtn" onclick="edit(this.id);">Edit</button>
+        `;
         let deleteBtn = `<span class="toooltiptext">
-        <button id =""class="btn btn-danger delbtn" onclick="deletfunc(this.id)">Delete</button>
+        <button id ="" class="btn btn-danger delbtn" onclick="deletfunc(this.id);">Delete</button>
+        ${editBtn}
         </span>`;
+        
         
         //for the electronic name
         let elect = document.createElement('span');
         elect.innerHTML = electName;
-        elect.className = 'left';
+        //elect.id = 'electron';
+        elect.className = 'left electname';
 
         //for the individual power vairable
         let powercount = document.createElement('span');
         powercount.innerHTML = `${power}watts ${deleteBtn}`;
-        powercount.className = 'right';
+        powercount.className = 'right watt';
     
      
         
@@ -129,10 +147,17 @@ function powersum(){
  function assignId(){
     let idnum = 0;
     let cont = document.querySelectorAll('.cont');
+    let editbtn = document.querySelectorAll('.editbtn');
     let butts = document.querySelectorAll('.delbtn');
+    let electname = document.querySelectorAll('.electname');
+    let watt = document.querySelectorAll('.watt');
+
     for (let i = 0; i < butts.length; i++){
         butts[i].id = idnum;
         cont[i].id = idnum;
+        editbtn[i].id = idnum;
+        electname[i].id = `electname${idnum}`;
+        watt[i].id = `watt${idnum}`;
 
         idnum ++;
     }
@@ -164,7 +189,7 @@ function calcu_function(){
 function deletfunc(x){
     //powersum();
     //calcu_function();
-    let id = `${x}`
+    let id = `${x}`;
     let ids = document.getElementById(id);
     let idnumeber = ids.id ;
     let re = parseInt(idnumeber);
@@ -184,5 +209,68 @@ function deletfunc(x){
     powersum();
 }
 
+//strickly for save button only
+let saveval = 0;
+function edit(y){
+    
+
+    inputDiv.classList.add('hidden');
+    editInputDiv.classList.remove('hidden');
+    //assignId();
+    let id = y;
+    let ids = document.getElementById(id);
+    let idnumeber = ids.id ;
+    let re = parseInt(idnumeber);
+    let electname = document.getElementById(`electname${re}`);
+    let watt = document.getElementById(`watt${re}`);
+
+    elect_edit.value = electname.innerHTML;
+    power_edit.value = powerObj[re].power;
+    hours_edit.value = powerObj[re].hours;
+    
+    assignId();
+    return saveval = re;
+    //for debugging purpose
+    //console.log(id);
+    //console.log(re);
+    //console.log('the edit feature function');
+
+    //console.log(electname);
+    //console.log(watt);
+    
+}
+
+function edit_save(){
+    let electname = document.getElementById(`electname${saveval}`);
+    let watt = document.getElementById(`watt${saveval}`);
+
+    let editBtn = `
+    <button id = "" class="btn btn-danger editbtn" onclick="edit(this.id);">Edit</button>
+    `;
+    let deleteBtn = `<span class="toooltiptext">
+    <button id ="" class="btn btn-danger delbtn" onclick="deletfunc(this.id);">Delete</button>
+    ${editBtn}
+    </span>`;
+
+
+    electname.innerHTML = `${elect_edit.value}`;
+    powerObj[saveval].power = parseInt(power_edit.value);
+    powerObj[saveval].hours = parseFloat(hours_edit.value);
+
+    watt.innerHTML = `${power_edit.value}watts${deleteBtn}`;
+    //electname.innerHTML = elect_edit.value;
+    //power_edit.value = powerObj[re].power;
+   // hours_edit.value = powerObj[re].hours;
+    inputDiv.classList.remove('hidden');
+    editInputDiv.classList.add('hidden');
+    assignId();
+    powersum();
+    elect_edit.value = '';
+    power_edit.value = '';
+    hours_edit.value = '';
+
+}
+
 //events listeners right here 
 add_btn.addEventListener('click', addItems);
+edit_btn.addEventListener('click', edit_save);
